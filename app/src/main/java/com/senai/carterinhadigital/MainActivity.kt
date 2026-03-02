cf28f818561e67eec8ffcdbf621c8c199f62bb82
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2026-03-02 09:22:19 -0300

    style: improve layout spacing and component alignment

--- app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
@@ -5,26 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rafaelcosta.myapplication.QrCode
 import com.senai.carterinhadigital.ui.theme.CarterinhaDigitalTheme
 
@@ -60,6 +68,7 @@ fun CarterinhaDigitalApp( modifier: Modifier = Modifier ){
     Column(
         modifier = modifier,
         horizontalAlignment =  Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ){
         Image(
             painter = painterResource(R.drawable.logosenai),
@@ -67,7 +76,7 @@ fun CarterinhaDigitalApp( modifier: Modifier = Modifier ){
             modifier= Modifier
                 .weight(.5f)
             .padding(top= 10.dp)
-            .fillMaxWidth(.7f)
+            .fillMaxWidth(.7f) // fração
 
 
 
@@ -78,29 +87,48 @@ fun CarterinhaDigitalApp( modifier: Modifier = Modifier ){
                     contentDescription = "Foto de Perfil",
             contentScale = ContentScale.Crop,
             modifier= Modifier
-                .weight(.2f)
+                .weight(.5f)
                 //.size(200.dp)
                 .clip(CircleShape)
                 .aspectRatio(1f)
                 .fillMaxWidth(.8f)
+                .background(color = Color.Red)
+        )
+        Spacer(
+            modifier = Modifier
+                .weight(.3f)
         )
 
-    Row(modifier= Modifier.weight(.2f)
+    Row(modifier= Modifier.weight(.5f)
             ){
-            Text( "Nome")
+            Text( "Nome: ")
             Text("Gabrieli da Silva Marcelino")
         }
         Row(modifier= Modifier.
         weight(3f)
             .fillMaxWidth(.6f)
         ){
-            Text("Curso")
-            Text("Tecnico Análise de Desenvolvimento de Sistemas")
+            Text("Curso: ",
+            textAlign = TextAlign.Right,
+            style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f)
+            )
+
+            Text( "Tecnico Análise de Desenvolvimento de Sistemas",
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontFamily = MaterialTheme.typography.titleLarge.fontFamily,
+                lineHeight = MaterialTheme.typography.titleLarge.lineHeight,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .weight(4f),
+                softWrap = true,
+
+            )
 
         }
         QrCode("90000000001417015720",
             modifier= Modifier
-                .weight(2f)
+                .weight(3f)
                 .fillMaxWidth(.6f)
                 //.clip()
 
