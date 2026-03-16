b9e6a7768b1ae2db5ca27b652125df026ec4b322
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2026-03-16 10:52:51 -0300

    refactor: organização do código e correção de pequenos problemas
    - Reorganização da estrutura do projeto
    - Remoção de imports e variáveis não utilizados
    - Ajustes de formatação
    - Correção de pequenos erros no código

--- app/src/main/java/com/senai/carterinhadigital/app/App.kt
+++ app/src/main/java/com/senai/carterinhadigital/app/App.kt
@@ -1,6 +1,8 @@
 package com.senai.carterinhadigital.app
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.senai.carterinhadigital.core.designsystem.navigation.AppNavHost
 
 @Composable
 fun App(){
